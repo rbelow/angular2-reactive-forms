a9b38d478b3f2c18908d44faff6ebb3c81dc6f67
Ruben Below <rbelow@hagafu.com>
2018-05-21 22:44:52 +0200

    basic reactive form model

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/template-driven/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit {
+    customerForm: FormGroup;
     // we use a class and not an interface because we want to create
     // a new instance of the class for the new customer information
-    customer: Customer= new Customer();
+    customer: Customer= new Customer(); // data model defines the data passed from/to our back-end server. Data model !== Form model
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    ngOnInit(): void {
+        // `AbstractControl` is the base class for `FormControl` and `FormGroup`
+        this.customerForm = new FormGroup({
+            // Form Model that tracks form value and state
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            // set/pass default input values through the `FormControl` constructor
+            sendCatalog: new FormControl(true)
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
     }
 }
