602439f916fa51fb3e15f3377f95f6badb593411
Ruben Below <rbelow@hagafu.com>
2018-05-22 23:38:50 +0200

    form builder

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+// import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
@@ -12,16 +13,29 @@ export class CustomerComponent implements OnInit {
     // a new instance of the class for the new customer information
     customer: Customer= new Customer(); // data model defines the data passed from/to our back-end server. Data model !== Form model
 
+    constructor(private fb: FormBuilder) { }
+
     ngOnInit(): void {
-        // `AbstractControl` is the base class for `FormControl` and `FormGroup`
-        this.customerForm = new FormGroup({
-            // Form Model that tracks form value and state
-            firstName: new FormControl(),
-            lastName: new FormControl(),
-            email: new FormControl(),
+        // FormBuilder allows us to create directly FormControl's and FormGroup's without explicitly defining them
+        // e. g. `this.fb.group({senCatalog: {value: true, disabled: false}})`
+        // e. g. `this.fb.group([{senCatalog: {value: true, disabled: false}, validation]})`        
+        this.customerForm = this.fb.group({
+            firstName: '',
+            // lastName: {value: 'n/a', disabled: true},
+            lastName: '',
+            email: '',
             // set/pass default input values through the `FormControl` constructor
-            sendCatalog: new FormControl(true)
+            sendCatalog: true
         });
+        // `AbstractControl` is the base class for `FormControl` and `FormGroup`
+        // this.customerForm = new FormGroup({
+        //     // Form Model that tracks form value and state
+        //     firstName: new FormControl(),
+        //     lastName: new FormControl(),
+        //     email: new FormControl(),
+        //     // set/pass default input values through the `FormControl` constructor
+        //     sendCatalog: new FormControl(true)
+        // });
     }
 
     // set all form data with setValue()
